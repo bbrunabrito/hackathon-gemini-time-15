55877fe6a0d9194170089b995f5eb658300c9b68
gabysouza1105 <gaby.souza1105@gmail.com>
2022-09-24 15:13:08 -0300

    implementaçao cardapio

--- src/pages/Cardapio/CardapioPage.jsx
+++ src/pages/Cardapio/CardapioPage.jsx
@@ -5,29 +5,25 @@ import { getDetalhes } from "../../services/detalhes.service";
 function CardapioPage() {
     const [cardapio, setCardapio] = useState([{
         categoria: "",
-        itens: [],
+        itens: [{}],
     }]);
 
     useEffect(() => {
         getDetalhes(0).then((data) => {
             setCardapio(data.cardapio);
-            console.log(cardapio);
         });
     }, []);
-
+    
     return (
         <div className="categoriaComida">
-            {/* <h2>{cardapio.categoria}</h2> */}
+            <h2>{cardapio[0].categoria}</h2>
             <section>
-                {/* {cardapio.map((item, i) => {
-                    <div className="item">
-                        <p>asdfghj</p>
-                        <img src={cardapio[0].itens[0].imagem} alt={cardapio[0].itens[0].nome} />
-                        <h3>{cardapio[0].itens[0].nome}</h3>
-                        <h4>{cardapio[0].itens[0].descricao}</h4>
-                        <span className="preco">{cardapio[0].itens[0].valor}</span>
-                    </div>
-                })}  */}
+                <div className="item">
+                    <img src={cardapio[0].itens[0].imagem} alt={cardapio[0].itens[0].nome} />
+                    <h3>{cardapio[0].itens[0].nome}</h3>
+                    <h4>{cardapio[0].itens[0].descricao}</h4>
+                    <span className="preco">{cardapio[0].itens[0].valor}</span>
+                </div>
             </section>
         </div>
     )
