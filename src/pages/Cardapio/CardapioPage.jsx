9f58b3362e47ae4a7a95fd3616748e120d5d2488
gabysouza1105 <gaby.souza1105@gmail.com>
2022-09-24 16:41:18 -0300

    alteração do conflito

--- src/pages/Cardapio/CardapioPage.jsx
+++ src/pages/Cardapio/CardapioPage.jsx
@@ -19,10 +19,6 @@ function CardapioPage() {
             <h2>{cardapio[0].categoria}</h2>
             <section>
                 <div className="item">
-                    <img src={cardapio[0].itens[0].imagem} alt={cardapio[0].itens[0].nome} />
-                    <h3>{cardapio[0].itens[0].nome}</h3>
-                    <h4>{cardapio[0].itens[0].descricao}</h4>
-                    <span className="preco">{cardapio[0].itens[0].valor}</span>
                 </div>
             </section>
         </div>
