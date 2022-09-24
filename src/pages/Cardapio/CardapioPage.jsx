55dfbe144fedf9b735bb72de83dfe6bf0865f0c3
gabysouza1105 <gaby.souza1105@gmail.com>
2022-09-24 15:11:11 -0300

    implementacao cardapio

--- src/pages/Cardapio/CardapioPage.jsx
+++ src/pages/Cardapio/CardapioPage.jsx
@@ -5,26 +5,24 @@ import { getDetalhes } from "../../services/detalhes.service";
 function CardapioPage() {
     const [cardapio, setCardapio] = useState([{
         categoria: "",
-        itens: [],
+        itens: [{}],
     }]);
 
     useEffect(() => {
         getDetalhes(0).then((data) => {
-            setCardapio(data);
-            console.log(data);
+            setCardapio(data.cardapio);
         });
     }, []);
-
+    
     return (
         <div className="categoriaComida">
-            {/* <h2>{cardapio.categoria}</h2> */}
+            <h2>{cardapio[0].categoria}</h2>
             <section>
                 <div className="item">
-                    <p>asdfghj</p>
-                    {/* <img src={cardapio[0].itens[0].imagem} alt={cardapio[0].itens[0].nome} />
+                    <img src={cardapio[0].itens[0].imagem} alt={cardapio[0].itens[0].nome} />
                     <h3>{cardapio[0].itens[0].nome}</h3>
                     <h4>{cardapio[0].itens[0].descricao}</h4>
-                    <span className="preco">{cardapio[0].itens[0].valor}</span> */}
+                    <span className="preco">{cardapio[0].itens[0].valor}</span>
                 </div>
             </section>
         </div>
